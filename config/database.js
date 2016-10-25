819b5b33faedb02bcda37d530f9ef6f52e545383
abhi608 <608abhishek@gmail.com>
2016-10-26 02:21:10 +0530

    Basic Working model

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'vanshika608'
+    },
+    'database': 'shareCapital',
+    'users_table': 'users'
+};
