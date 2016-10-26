69e8a2ddda79bc97c2d00e7ca0dd30fe84200467
abhi608 <608abhishek@gmail.com>
2016-10-26 11:38:17 +0530

    username changed to Email

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'vanshika608'
+        'password': 'YOUR_PASSWORD_HERE'
     },
     'database': 'shareCapital',
     'users_table': 'users'
